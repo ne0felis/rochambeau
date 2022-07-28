16fd44e726918b78b914613fb8bcfff64ceed7fc
ne0felis <akbar.risanta@gmail.com>
2022-07-28 20:19:51 +0700

    Create a rock paper scissors game in console

--- index.js
+++ index.js
@@ -1,23 +1,73 @@
+function getPlayerChoice() {
+  while (Infinity) {
+    let options = ["rock", "paper", "scissors"];
+    let choice = prompt("Enter your choice: rock, paper, or scissors");
+    choice = choice.toLowerCase();
+    if (choice === "rock" || choice === "paper" || choice === "scissors") {
+      return choice;
+    }
+  }
+}
+
 function getComputerChoice() {
-    let options = ["Rock", "Paper", "Scissors"];
-    let choice = options[Math.floor(Math.random()*options.length)];
-    return choice;
+  let options = ["rock", "paper", "scissors"];
+  let choice = options[Math.floor(Math.random() * options.length)];
+  return choice;
 }
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
-    computerSelection = computerSelection.toLowerCase();
+  playerSelection = playerSelection.toLowerCase();
+  computerSelection = computerSelection.toLowerCase();
 
-    if (playerSelection === computerSelection) {
-        return `It's a tie! ${playerSelection} ties with ${computerSelection}`;
-    } else if ((playerSelection === 'rock' && computerSelection === 'scissors') || 
-                    (playerSelection === 'paper' && computerSelection === 'rock') || 
-                    (playerSelection === 'scissors' && computerSelection === 'paper')) {
-        return `It's a win! ${playerSelection} beats ${computerSelection}`;
-    } else {
-        return `It's a lost! ${playerSelection} loses to ${computerSelection}`;
-    }
+  if (playerSelection === computerSelection) {
+    return "tie";
+  } else if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    return `win`;
+  } else {
+    return `lose`;
+  }
 }
+
+// It's a lost! ${playerSelection} loses to ${computerSelection)
+
+function game() {
+  playerScore = 0;
+  computerScore = 0;
+  roundStatus = "";
+  playerSelection = getPlayerChoice();
+  computerSelection = getComputerChoice();
+
+  for (let i = 0; i < 5; i++) {
+    roundStatus = playRound(playerSelection, computerSelection);
+
+    if (roundStatus === "win") {
+      playerScore++;
+      console.log(`It's a win! ${playerSelection} beats ${computerSelection}`);
+    }
+
+    if (roundStatus === "lose") {
+      playerScore--;
+      console.log(
+        `It's a lost! ${playerSelection} loses to ${computerSelection}`
+      );
+    }
+
+    if (roundStatus === "tie") {
+      console.log(
+        `It's a tie! ${playerSelection} ties with ${computerSelection}`
+      );
+    }
+
+    playerSelection = getPlayerChoice();
+    computerSelection = getComputerChoice();
+  }
+}
+
+game(getComputerChoice(), getComputerChoice());
 
 /*
 Problem Framework
@@ -34,7 +84,8 @@ Plan:
 
 Divide:
     Get the player choice
-
+    - Use prompt() to get input from the user
+    - Get input from the user again if it is not one of the options
     
     Get the randomized computer's choice
         - Create an array containing the options which is rock, paper, and scissors string
